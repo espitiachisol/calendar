c4e5f963b18a0aa1cc907cd3cfe952c416700982
espitiachisol <espitiachisol@gmail.com>
2024-08-07 16:27:39 +0800

    feat(Calendar): complete component functionality with cross-month range selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useState } from 'react';
 import Calendar from './calendar/Calendar';
-
 function App() {
-  return <Calendar />;
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  function handleSelectRange(dates) {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  }
+
+  return (
+    <Calendar
+      onSelectRange={handleSelectRange}
+      startDate={startDate}
+      endDate={endDate}
+    />
+  );
 }
 export default App;
--- src/calendar/Calendar.tsx
+++ src/calendar/Calendar.tsx
@@ -1,18 +1,183 @@
+import {
+  addDays,
+  addMonths,
+  endOfMonth,
+  format,
+  getDay,
+  getDaysInMonth,
+  getTime,
+  isAfter,
+  isBefore,
+  isSameDay,
+  isSameMonth,
+  isToday,
+  startOfMonth,
+  subDays,
+  subMonths,
+} from 'date-fns';
+import { zhTW } from 'date-fns/locale';
 import styles from './Calendar.module.css';
+import { useState } from 'react';
 
-export default function Calendar() {
+function Month({ children }) {
+  return <section className={styles.month}>{children}</section>;
+}
+
+function MonthDates({
+  dates,
+  startDate,
+  endDate,
+  handleClickDay,
+  currentMonth,
+  locale,
+}) {
+  return dates.map((date) => {
+    return (
+      <Day
+        key={getTime(date)}
+        date={date}
+        startDate={startDate}
+        endDate={endDate}
+        onClickDay={handleClickDay}
+        currentMonth={currentMonth}
+      >
+        {format(date, 'do', { locale })}
+      </Day>
+    );
+  });
+}
+
+function Day({ date, currentMonth, startDate, endDate, onClickDay, children }) {
+  let className = `${styles.day} `;
+  if (isToday(date)) {
+    className += `${styles.highlight} `;
+  }
+  if (!isSameMonth(date, currentMonth)) {
+    className += `${styles.muted} `;
+  }
+
+  if (startDate) {
+    if (isSameDay(date, startDate)) {
+      className += `${styles.selected} `;
+    }
+  }
+  if (endDate) {
+    if (isSameDay(date, endDate)) {
+      className += `${styles.selected} `;
+    } else if (isAfter(date, startDate) && isBefore(date, endDate)) {
+      className += `${styles.selected} `;
+    }
+  }
+
+  return (
+    <button
+      key={getTime(date)}
+      className={className}
+      onClick={() => onClickDay(date)}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default function Calendar({
+  onSelectRange,
+  startDate,
+  endDate,
+  locale = zhTW,
+}) {
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  const firstDayOfCurrentMonth = startOfMonth(currentMonth);
+  const totalDaysInCurrentMonth = getDaysInMonth(currentMonth);
+  const weekStartsOn = locale.options?.weekStartsOn ?? 1;
+
+  const daysInWeek = 7;
+  const daysFromPreviousMonth =
+    (getDay(firstDayOfCurrentMonth) - weekStartsOn + daysInWeek) % daysInWeek;
+  const totalCalendarDays =
+    Math.ceil((daysFromPreviousMonth + totalDaysInCurrentMonth) / daysInWeek) *
+    daysInWeek;
+  const daysFromNextMonth =
+    totalCalendarDays - (daysFromPreviousMonth + totalDaysInCurrentMonth);
+
+  const currentMonthDates = Array.from(
+    { length: totalDaysInCurrentMonth },
+    (_, i) => addDays(firstDayOfCurrentMonth, i)
+  );
+  const previousMonthDates = Array.from(
+    { length: daysFromPreviousMonth },
+    (_, i) => subDays(firstDayOfCurrentMonth, i + 1)
+  ).reverse();
+
+  const lastDayOfCurrentMonth = endOfMonth(currentMonth);
+  const nextMonthDates = Array.from({ length: daysFromNextMonth }, (_, i) =>
+    addDays(lastDayOfCurrentMonth, i + 1)
+  );
+
+  function handlePreviousButtonClick() {
+    setCurrentMonth((date) => subMonths(date, 1));
+  }
+
+  function handleNextButtonClick() {
+    setCurrentMonth((date) => addMonths(date, 1));
+  }
+
+  function handleClickDay(date: Date) {
+    let start = startDate || null;
+    let end = endDate || null;
+    if (!start) {
+      start = date;
+    } else if (!end && (isSameDay(date, start) || isAfter(date, start))) {
+      end = date;
+    } else {
+      start = date;
+      end = null;
+    }
+    onSelectRange([start, end]);
+  }
+
   return (
     <article className={styles.calendar}>
       <header className={styles.header}>
-        <button className={styles.selectMonth}>&lt;</button>
-        <time className={styles.currentMonth}> 2023 年 6 月</time>
-        <button className={styles.selectMonth}>&gt;</button>
+        <button
+          className={styles.selectMonth}
+          onClick={handlePreviousButtonClick}
+        >
+          &lt;
+        </button>
+        <time className={styles.currentMonth}>
+          {format(currentMonth, 'yyyy年M月', { locale })}
+        </time>
+        <button className={styles.selectMonth} onClick={handleNextButtonClick}>
+          &gt;
+        </button>
       </header>
-      <section className={styles.month}>
-        {Array.from({ length: 40 }, (_, i) => i + 1).map((day) => {
-          return <button className={styles.day}>{day}</button>;
-        })}
-      </section>
+      <Month>
+        <MonthDates
+          dates={previousMonthDates}
+          startDate={startDate}
+          endDate={endDate}
+          handleClickDay={handleClickDay}
+          locale={locale}
+          currentMonth={currentMonth}
+        />
+        <MonthDates
+          dates={currentMonthDates}
+          startDate={startDate}
+          endDate={endDate}
+          handleClickDay={handleClickDay}
+          locale={locale}
+          currentMonth={currentMonth}
+        />
+        <MonthDates
+          dates={nextMonthDates}
+          startDate={startDate}
+          endDate={endDate}
+          handleClickDay={handleClickDay}
+          locale={locale}
+          currentMonth={currentMonth}
+        />
+      </Month>
     </article>
   );
 }
